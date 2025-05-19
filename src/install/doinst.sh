3022b8089518fede2d291967278d2c588eaeac2a
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-05-19 21:41:17 +0000

    fix: package files

--- src/install/doinst.sh
+++ src/install/doinst.sh
@@ -6,8 +6,8 @@ if [ ! -f /boot/config/plugins/enhanced.log/custom_syslog.conf ]; then
   cp -a /usr/local/emhttp/plugins/enhanced.log/default/custom_syslog.conf /boot/config/plugins/enhanced.log/custom_syslog.conf
 fi
 
-if [ ! -f /boot/config/plugins/enhanced.log/syslog_filter.cfg ]; then
-  cp -a /usr/local/emhttp/plugins/enhanced.log/default/syslog_filter.cfg /boot/config/plugins/enhanced.log/syslog_filter.cfg
+if [ ! -f /boot/config/plugins/enhanced.log/syslog_filter.conf ]; then
+  cp -a /usr/local/emhttp/plugins/enhanced.log/default/syslog_filter.conf /boot/config/plugins/enhanced.log/syslog_filter.conf
 fi
 
 /usr/local/emhttp/plugins/enhanced.log/scripts/rc.enhanced.log
