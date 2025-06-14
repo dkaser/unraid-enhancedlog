d1c5cda0784e93df983055d0d6876c80b2672a1c
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-06-14 04:49:30 +0000

    feat: switch to datatables

--- src/usr/local/emhttp/plugins/enhanced.log/include/Pages/EnhancedSyslog.php
+++ src/usr/local/emhttp/plugins/enhanced.log/include/Pages/EnhancedSyslog.php
@@ -33,7 +33,6 @@ $tr = $tr ?? new Translator(PLUGIN_ROOT);
 $logs = Utils::getLogFiles();
 
 $enhanced_log_cfg = Utils::getConfig();
-$maxLines         = intval(isset($enhanced_log_cfg['LINES']) && $enhanced_log_cfg['LINES'] != "" ? $enhanced_log_cfg['LINES'] : 1000);
 $colors           = new Colors();
 $colors->parseConfig($enhanced_log_cfg);
 
@@ -45,109 +44,40 @@ if (in_array($theme ?? "", $themeArray)) {
 }
 
 ?>
-<link type="text/css" rel="stylesheet" href="/plugins/enhanced.log/assets/style.css">
-<script src="/webGui/javascript/jquery.tablesorter.widgets.js"></script>
-
-<!-- Select2 code -->
-<link href="/plugins/enhanced.log/assets/select2.min.css" rel="stylesheet">
-<script src="/plugins/enhanced.log/assets/select2.min.js"></script>
-<script src="/plugins/enhanced.log/assets/widget-filter-formatter-select2.js"></script>
-
-<div>
-<select name="logLog" onchange='showLog()'>
-<?php foreach ($logs as $file) {
-    echo Utils::make_option(false, $file, basename($file));
-} ?>
-</select>
-<?= $tr->tr("max_lines"); ?>:
-<input type="number" name="logMax" value="" placeholder="<?= $maxLines; ?>">
-<input type="button" value="<?= $tr->tr("refresh"); ?>" onclick="showLog()">
-<input type="button" class="reset" value="<?= $tr->tr("reset"); ?>">
-</div>
-
-<table id='logTable' class="unraid logTable tablesorter"><tr><td><div class="spinner"></div></td></tr></table><br>
 
 <script>
-function showLog() {
-  var log = $('select[name="logLog"]').val();
-  var maxLines = $('input[name="logMax"]').val();
-  $.post('/plugins/enhanced.log/include/data/log.php', {log: log, maxLines: maxLines}, function(data){
-    clearTimeout(timers.refresh);
-    $("#logTable").trigger("destroy");
-    $('#logTable').html(data.html);
-    $('#logTable').tablesorter({
-      widthFixed : true,
-      sortList: [[0,1]],
-      widgets: ['stickyHeaders','filter','zebra'],
-      headers: {
-        0: { sorter: 'data' }, // date
-      },
-      widgetOptions: {
-        // on black and white, offset is height of #menu
-        // on azure and gray, offset is height of #header
-        stickyHeaders_offset: ($('#menu').height() < 50) ? $('#menu').height() : $('#header').height(),
-        filter_columnFilters: true,
-        filter_reset: '.reset',
-        filter_liveSearch: true,
-        filter_formatter : {
-            // Alphanumeric (match)
-            1: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : true,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-            2: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : false,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-            4: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : true,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-        },
-        zebra: ["normal-row","alt-row"]
-      },
+const logFiles = {
+    <?php foreach ($logs as $file) { ?>
+        "<?= basename($file); ?>": "<?= $file; ?>",
+    <?php } ?>
+};
+</script>
+<link type="text/css" rel="stylesheet" href="/plugins/enhanced.log/assets/style.css">
+<script src="/plugins/enhanced.log/assets/datatables.min.js"></script>
+<script src="/plugins/enhanced.log/assets/luxon.min.js"></script>
+<script src="/plugins/enhanced.log/assets/enhancedlog.js"></script>
+<link type="text/css" rel="stylesheet" href="/plugins/enhanced.log/assets/datatables.min.css">
 
-    });
-    $('div.spinner.fixed').hide('fast');
-    //controlsDisabled(false);
-  }, "json");
-}
+<table id='logTable' class="stripe compact">
+    <thead>
+        <tr>
+            <th><strong><?= $tr->tr("date"); ?></strong></th>
+            <th><strong><?= $tr->tr("source"); ?></strong></th>
+            <th><strong><?= $tr->tr("service"); ?></strong></th>
+            <th><strong><?= $tr->tr("message"); ?></strong></th>
+            <th><strong><?= $tr->tr("match"); ?></strong></th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+    </tfoot>
+</table>
 
-$(function() {
-  $.tablesorter.addParser({
-    // set a unique id
-    id: 'data',
-    is: function(s, table, cell, $cell) {
-      // return false so this parser is not auto detected
-      return false;
-    },
-    format: function(s, table, cell, cellIndex) {
-      var $cell = $(cell);
-      return $cell.attr('data-index') || s;
-    },
-    // flag for filter widget (true = ALWAYS search parsed values; false = search cell text)
-    parsed: false,
-    // set type, either numeric or text
-    type: 'numeric'
-  });
-
-  showLog();
-});
+<script>
+$(document).ready( function () {
+    $('#logTable').DataTable(getDatatableConfig('/plugins/enhanced.log/data.php/log', '<?= $tr->tr("refresh"); ?>', 'log'));
+} );
 
 <?php foreach ($colors->getColors() as $color) { ?>
 $('.tabs').append("<?= $colors->getColorTag($color, $font_size, $tr); ?>");
--- src/usr/local/emhttp/plugins/enhanced.log/include/Pages/Hash.php
+++ src/usr/local/emhttp/plugins/enhanced.log/include/Pages/Hash.php
@@ -27,88 +27,27 @@ if ( ! defined(__NAMESPACE__ . '\PLUGIN_ROOT') || ! defined(__NAMESPACE__ . '\PL
 
 $tr = $tr ?? new Translator(PLUGIN_ROOT);
 
-$logs             = Utils::getLogFiles();
-$enhanced_log_cfg = Utils::getConfig();
-$maxLines         = intval(isset($enhanced_log_cfg['LINES']) && $enhanced_log_cfg['LINES'] != "" ? $enhanced_log_cfg['LINES'] : 1000);
-
 ?>
 
-<div>
-<select name="hashLog" onchange='showLogHashes()'>
-<?php foreach ($logs as $file) {
-    echo Utils::make_option(false, $file, basename($file));
-} ?>
-</select>
-<?= $tr->tr("max_lines"); ?>:
-<input type="number" name="hashMax" value="" placeholder="<?= $maxLines; ?>">
-<input type="button" value="<?= $tr->tr("refresh"); ?>" onclick="showLogHashes()">
-<input type="button" class="resetHash" value="<?= $tr->tr("reset"); ?>">
-</div>
-
-<table id='hashTable' class="unraid hashTable tablesorter"><tr><td><div class="spinner"></div></td></tr></table><br>
+<table id='summaryTable' class="stripe compact">
+    <thead>
+        <tr>
+            <th><strong><?= $tr->tr("count"); ?></strong></th>
+            <th><strong><?= $tr->tr("source"); ?></strong></th>
+            <th><strong><?= $tr->tr("service"); ?></strong></th>
+            <th><strong><?= $tr->tr("message"); ?></strong></th>
+            <th><strong><?= $tr->tr("match"); ?></strong></th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+    </tfoot>
+</table>
 
 <script>
-function showLogHashes() {
-  var log = $('select[name="hashLog"]').val();
-  var maxLines = $('input[name="hashMax"]').val();
-  $.post('/plugins/enhanced.log/include/data/hash.php',{log: log, maxLines: maxLines},function(data){
-    clearTimeout(timers.refresh);
-    $("#hashTable").trigger("destroy");
-    $('#hashTable').html(data.html);
-    $('#hashTable').tablesorter({
-      widthFixed : true,
-      sortList: [[0,1]],
-      widgets: ['stickyHeaders','filter','zebra'],
-      headers: {
-        0: { sorter: 'data' }, // date
-      },
-      widgetOptions: {
-        // on black and white, offset is height of #menu
-        // on azure and gray, offset is height of #header
-        stickyHeaders_offset: ($('#menu').height() < 50) ? $('#menu').height() : $('#header').height(),
-        filter_columnFilters: true,
-        filter_reset: '.resetHash',
-        filter_liveSearch: true,
-        filter_formatter : {
-            // Alphanumeric (match)
-            1: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : true,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-            2: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : false,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-            4: function($cell, indx) {
-            return $.tablesorter.filterFormatter.select2( $cell, indx, {
-                match : true,         // adds "filter-match" to header
-                multiple: true,      // allow multiple selections
-                width: '166px',
-                allowClear: true,
-                dropdownAutoWidth: true,
-            });
-            },
-        },
-        zebra: ["normal-row","alt-row"]
-      },
+$(document).ready( function () {
+    $('#summaryTable').DataTable(getSummaryConfig('/plugins/enhanced.log/data.php/summary', '<?= $tr->tr("refresh"); ?>', 'summary'));
+} );
 
-    });
-    $('div.spinner.fixed').hide('fast');
-    //controlsDisabled(false);
-  }, "json");
-}
-
-$(function() {
-  showLogHashes();
-});
 </script>
